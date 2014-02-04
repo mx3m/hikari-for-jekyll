6894de663d78a6a564e9e068398c76f99ff8adac
julien.rousseau <julien.rousseau@jivesoftware.com>
2014-02-03 18:32:59 -0800

    removed jQuery dependency

--- js/main.js
+++ js/main.js
@@ -1,27 +1,52 @@
-$(document).ready(function(){
+//TODO: implement an equivalent to $(document.ready)
+//window.onload will do the job for now.
+window.onload = function() {
 
-	// Off-canvas container toggle button
-	$('.menu-icon').click(function(){
-  		$(this).toggleClass('close');
-  		$('#off-canvas').toggleClass('toggled');
-	});
+    var $menuIcon = document.getElementsByClassName("menu-icon")[0],
+        $offCanva = document.getElementById('off-canvas');
 
-	// Open Twitter/share in a Pop-Up
-	  $('.popup').click(function(event) {
-	    var width  = 575,
-	        height = 400,
-	        left   = ($(window).width()  - width)  / 2,
-	        top    = ($(window).height() - height) / 2,
-	        url    = this.href,
-	        opts   = 'status=1' +
-	                 ',width='  + width  +
-	                 ',height=' + height +
-	                 ',top='    + top    +
-	                 ',left='   + left;
-	    
-	    window.open(url, 'twitter', opts);
-	 
-	    return false;
-	  });
+    $menuIcon.addEventListener('click', function() {
+        toggleClass($menuIcon, 'close');
+        toggleClass($offCanva, 'toggled');
+    }, false);
 
-});
+    function toggleClass(element, className) {
+        if (!element || !className) {
+            return;
+        }
+
+        var classString = element.className,
+            nameIndex = classString.indexOf(className);
+
+        if (nameIndex == -1) {
+            classString += ' ' + className;
+        }
+        else {
+            classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
+        }
+        element.className = classString;
+    }
+
+    // Open Twitter/share in a Pop-Up
+    var $popup = document.getElementsByClassName("popup")[0];
+    if (!$popup) {
+        return;
+    }
+    $popup.addEventListener('click', function(e) {
+        e.preventDefault()
+        var width  = 575,
+            height = 400,
+            left   = (document.documentElement.clientWidth  - width)  / 2,
+            top    = (document.documentElement.clientHeight - height) / 2,
+            url    = this.href,
+            opts   = 'status=1' +
+                     ',width='  + width  +
+                     ',height=' + height +
+                     ',top='    + top    +
+                     ',left='   + left;
+
+        window.open(url, 'twitter', opts);
+
+        return false;
+    });
+}

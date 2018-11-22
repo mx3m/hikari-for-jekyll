cf163dc2895f2b562b990d180bf5eb9c451aa6c1
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-11-22 16:08:50 +0100

    Fjernet ting jeg ikke trenger

--- Gemfile
+++ Gemfile
@@ -1,7 +1,3 @@
 source 'https://rubygems.org'
 
 gem 'jekyll', '~> 3.8'
-
-group :jekyll_plugins do
-  gem 'jekyll-paginate-v2', '~> 2.0'
-end
--- js/main.js
+++ js/main.js
@@ -40,27 +40,4 @@ window.onload = function() {
             removeClass(element, className);
         }
     }
-
-    // Open Twitter/share in a Pop-Up
-    var $popup = document.getElementsByClassName('popup')[0];
-    if (!$popup) {
-        return;
-    }
-    $popup.addEventListener('click', function(e) {
-        e.preventDefault()
-        var width  = 575,
-            height = 400,
-            left   = (document.documentElement.clientWidth  - width)  / 2,
-            top    = (document.documentElement.clientHeight - height) / 2,
-            url    = this.href,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-
-        window.open(url, 'twitter', opts);
-
-        return false;
-    });
 }
